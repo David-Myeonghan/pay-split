60b2963f6e878b2371d638c7efea55d9668543df
davidRyu <david.ryu@imagoworks.ai>
2023-05-04 00:40:03 +0900

    feat: config jest.config.ts for toHaveAttribute

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type {Config} from 'jest';
 const config: Config = {
   verbose: true,
   testEnvironment: "jsdom",
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 };
 
 export default config;
